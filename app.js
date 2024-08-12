6f0b9bb412603926e2b885982af6ffe914f263be
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-08-12 12:55:04 +0000

    A basic Example

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//setup static and middleware
+app.use(express.static("./public"))
 
 app.get("/", (req, res) => {
-    res.send("Home Page")
-})
-
-app.get("/about", (req, res) => {
-    res.send("About Page")
-})
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
 app.all("*", (req, res) => {
-    res.status(404).send("resource not found")
-})
+  res.status(404).send("resource not found");
+});
 
-app.listen(5000, ()=>{
-    console.log("server is listening on port 5000")
-})
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.listen
-// app.patch
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
